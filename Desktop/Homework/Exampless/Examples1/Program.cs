0883306f9b1fef4e92af445f1acc75cb62a9b94a
Sergey <sergey.kurilov@mail.ru>
2023-02-01 20:38:18 +0300

    Examples1

---
+++ Desktop/Homework/Exampless/Examples1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите первое число: ");
+int.TryParse(Console.ReadLine() , out int a );
+Console.Write("Введите второе число: ");
+int.TryParse(Console.ReadLine() , out int b );
+
+if (a > b)
+{
+    Console.Write("Max = ");
+    Console.Write(a);
+}
+else
+{
+   Console.Write("Max = ");
+   Console.Write(b);
+}

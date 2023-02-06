b2b8b6d2e07532bfec4e9a6e62083679bbc500ae
Sergey <sergey.kurilov@mail.ru>
2023-02-06 09:14:53 +0300

    Exx6

---
+++ Desktop/Homework/Exampless/Examples6/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число: ");
+int.TryParse(Console.ReadLine() , out int a);
+string sum = Convert.ToString(a);
+if (a<99)
+{
+    Console.Write("Число не 3-х значное,попробуйtе снова");
+}
+//int sum =0;
+//if (a > 99)
+//{
+//    sum = a %10 ;
+//}
+//if ( a<99)
+//{
+//    Console.Write("Число не 3-х значное");
+//}
+
+Console.Write(sum[2]);

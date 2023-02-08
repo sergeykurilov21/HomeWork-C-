4290778dbe9048cea3ad1a41f81dafd0db8932c1
Sergey <sergey.kurilov@mail.ru>
2023-02-08 13:07:56 +0300

    Exx23

---
+++ Desktop/Homework/Exampless/Examples23/Program.cs
@@ -0,0 +1,27 @@
+﻿
+Colculate(ReadInt("number"));
+
+int ReadInt(string argument)
+{
+    Console.Write($"Введите {argument}:");
+    int number;
+
+    while(!int.TryParse(Console.ReadLine(),out number))
+    {
+        Console.WriteLine("Это не число");
+    }
+
+    return number;
+           
+}  
+
+int Colculate(int number)
+{
+    int sum = 0;
+
+    for (int i = 1; i<=number ; i++)
+    {
+       Console.WriteLine(Math.Pow(i,3));
+    }
+    return sum;
+}

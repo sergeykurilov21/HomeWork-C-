8a73dbde1a7e87b43f24cd460c12a7d60c80c6aa
Sergey <sergey.kurilov@mail.ru>
2023-02-08 11:13:41 +0300

    Examples19

---
+++ Desktop/Homework/Exampless/Examples19/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число: ");
+int.TryParse(Console.ReadLine() , out int n);
+int temp = n;
+int rev = 0;
+while(n > 0)
+{
+    int dig = n % 10;
+    rev = rev * 10 + dig;
+    n = n / 10;
+}
+if(temp == rev)
+{
+    Console.Write("Это палиндром!");
+}
+else
+{
+    Console.Write("Это не палиндром!");
+}

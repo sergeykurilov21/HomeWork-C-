6a6c71a39deb7fbd88922ff3d4997d4cbe689cc8
Sergey <sergey.kurilov@mail.ru>
2023-02-12 19:56:34 +0300

    Exx25

---
+++ Desktop/Homework/Exampless/Examples25/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int CalculateNumber(int number, int num){
+  int result = 1;
+  for(int i=1; i <= num; i++){
+    result = result * number;
+  }
+    return result;
+}
+
+  Console.Write("Введите число: ");
+  int number = Convert.ToInt16(Console.ReadLine());
+  Console.Write("Введите степень: ");
+  int num = Convert.ToInt16(Console.ReadLine());
+
+  int sum = CalculateNumber(number, num);
+  Console.WriteLine("Получилось: " + sum);

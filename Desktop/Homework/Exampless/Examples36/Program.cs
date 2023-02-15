fc2e869c13d4cc7766b9b190419226af4a468ef6
Sergey <sergey.kurilov@mail.ru>
2023-02-15 18:05:16 +0300

    Exx36

---
+++ Desktop/Homework/Exampless/Examples36/Program.cs
@@ -0,0 +1,27 @@
+﻿
+int [] array = new int [4];
+
+
+RandomArrayy(array);
+Console.WriteLine(string.Join(" " ,array));
+
+void RandomArrayy(int [] array)
+{
+    Random random = new Random();
+    int sumNum=0;
+    for(int i = 0; i<array.Length;i++)
+    {     
+        array[i]=random.Next(0,99);
+        if ((i%2)==0)
+        {
+            continue;
+        }
+        else
+        {
+            sumNum+=array[i];
+        }
+        
+     
+    }
+     Console.WriteLine($"Cумма чисел в нечетных позициях равна: {sumNum}") ;
+}

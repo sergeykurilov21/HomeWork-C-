c1f9a07f891f443b4815d972017995fc16235d3d
Sergey <sergey.kurilov@mail.ru>
2023-02-15 17:48:32 +0300

    Exx34

---
+++ Desktop/Homework/Exampless/Examples34/Program.cs
@@ -0,0 +1,25 @@
+﻿int [] array = new int [4];
+
+
+RandomArrayy(array);
+Console.WriteLine(string.Join(" " ,array));
+
+void RandomArrayy(int [] array)
+{
+    Random random = new Random();
+    int sumNum=0;
+    for(int i = 0; i<array.Length;i++)
+    {     
+
+        array[i]=random.Next(99,999);
+         if((array[i]%2)==0)
+        {
+            sumNum++;       
+        }
+        
+     
+    }
+     Console.WriteLine($"Четных чисел в масиве {sumNum}") ;
+}
+
+

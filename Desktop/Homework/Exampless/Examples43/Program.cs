2577b88c1f17f5695cf4f7f570cfa61bc4a4e1c3
Sergey <sergey.kurilov@mail.ru>
2023-02-18 18:07:36 +0300

    Exx43

---
+++ Desktop/Homework/Exampless/Examples43/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите значение точки b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение точки k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение точки b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение точки k1");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");

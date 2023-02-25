bb3d48b9e0a39c019a46685235a18334c85049e3
Sergey <sergey.kurilov@mail.ru>
2023-02-25 15:24:38 +0300

    Exx54

---
+++ Desktop/Homework/Exampless/Examples52/Program.cs
@@ -0,0 +1,51 @@
+﻿int firstLength = ReadInt("First Length");
+int secondLength = ReadInt("Second Length");
+
+int[,] array = CreateTwoDimensionArray(firstLength, secondLength);
+int[,] function = CalculateArray(firstLength, secondLength);
+
+Console.WriteLine(TwoDimensionArrayToString(array));
+Console.WriteLine(TwoDimensionArrayToString(function));
+
+int ReadInt(string argument)
+{
+	Console.Write($"Input {argument}: ");
+	int result = 0;
+
+	while (!int.TryParse(Console.ReadLine(), out result))
+	{
+		Console.WriteLine("Try again");
+	}
+
+	return result;
+}
+
+int [,] CreateTwoDimensionArray(int firstLength, int secondLength)
+{
+  int[,] result = new int[firstLength, secondLength];
+  Random rand = new Random();
+
+  for (int i = 0; i < result.GetLength(0); i++)
+  {
+    for (int j = 0; j < result.GetLength(1); j++)
+    {
+      result[i, j] = rand.Next(1,10);
+    }
+  }
+
+  return result;
+}
+
+void CalculateArray(int firstLength, int secondLength)
+{
+    double x =0;
+    
+    for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i,j]= array[i,j] + x;
+      x =x/secondLength;
+    }
+  }
+}
---
+++ Desktop/Homework/Exampless/Examples54/Program.cs
@@ -0,0 +1,77 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// 1. Задать масив с рандомными значениями
+// 2.Написать функцию заполнения массива
+// 2.1 Прописать условия заполнения по убыванию
+//3.Вывести функцию
+
+
+
+int ReadInt(string argument)
+{
+    Console.Write($"Input {argument} :");
+    int number;
+
+    while(!int.TryParse(Console.ReadLine(),out number))
+    {
+        Console.Write("Oooops,try again");
+    }
+    return number;
+}
+
+
+
+int[,] CreateTwoDimensionArray(int firstLength, int secondLength)
+{
+  int[,] array = new int[firstLength, secondLength];
+  Random random = new Random();
+
+  for (int i = 0; i < firstLength; i++)
+  {
+    for (int j = 0; j < secondLength; j++)
+    {
+      array[i, j] = random.Next(0, 10);
+    }
+  }
+  return array;
+
+}
+void ReserchNumberAndSwithHIs(int[,] array)
+{
+    int max = 0;
+    int count = 0;
+    for (int i = 0; i < array.GetLength(0)/2; i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {   
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }       
+        }
+    Console.WriteLine(array);
+}
+}
+
+void PrintArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write($"{array[i, j]} ");
+    }
+
+    Console.WriteLine();
+  }
+
+}
+int[,] array = CreateTwoDimensionArray(ReadInt("firstLength" ), ReadInt("secondLength"));
+PrintArray(array);
+
+ReserchNumberAndSwithHIs(array);
+PrintArray(array);

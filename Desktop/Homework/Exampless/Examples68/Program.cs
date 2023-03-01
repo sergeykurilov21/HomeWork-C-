714f2d525fc8f69d83e33c71a05fdb09f7ee56c4
Sergey <sergey.kurilov@mail.ru>
2023-03-01 19:42:14 +0300

    Exx68

---
+++ Desktop/Homework/Exampless/Examples68/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+    int AkkermanFunc(int m, int n)
+		{
+			if(m == 0)
+			{
+				return n + 1;
+			}
+			if(m > 0 && n == 0)
+			{
+				return AkkermanFunc(m - 1, 1);
+			}
+			return AkkermanFunc(m - 1, AkkermanFunc(m, n - 1));
+		}
+		
+		Console.WriteLine(AkkermanFunc(3,2));
+	}
+}

d60c21e2a952fa7717317e2dd10e71629fdafebe
Sergey <sergey.kurilov@mail.ru>
2023-03-01 19:38:54 +0300

    Exx66

---
+++ Desktop/Homework/Exampless/Examples66/Program.cs
@@ -0,0 +1,27 @@
+﻿
+int m = ReadInt("Введите M");
+int n = ReadInt("Введите N");
+
+
+Console.WriteLine($"Сумма элементов от {m} до {n} равна :{NaturalNumber(m,n)}");
+
+
+
+int ReadInt(string argument)
+{
+    Console.Write($"Input {argument} :");
+    int n;
+
+    while(!int.TryParse(Console.ReadLine(),out n))
+    {
+        Console.Write("Oooops,sorry");
+    }
+    return n;
+}
+
+int NaturalNumber(int m, int n)
+{
+    if (m == n)
+        return n;
+    return n + NaturalNumber(m, n - 1);
+}

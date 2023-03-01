7f98df2df0101db75865a118c84229b0e72fb17a
Sergey <sergey.kurilov@mail.ru>
2023-03-01 19:33:02 +0300

    Exx64

---
+++ Desktop/Homework/Exampless/Examples64/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+
+
+
+
+
+int n = ReadInt("N");
+
+int m = 1;
+
+if (n < 1)
+{
+    Console.WriteLine("Ввели не положительное число");
+}
+Console.WriteLine(NaturalNumber(n, m));
+
+
+int ReadInt(string argument)
+{
+    Console.Write($"Input {argument} :");
+    int n;
+
+    while(!int.TryParse(Console.ReadLine(),out n))
+    {
+        Console.Write("Oooops,sorry");
+    }
+    return n;
+}
+
+int NaturalNumber(int n, int m)
+{
+    if (n == m)
+        return n;
+    else
+        Console.Write($"{NaturalNumber(n, m + 1)}, ");
+    return m;
+}
+
